4c8c704e134ee4e1abc5b8312e51b712b8ead82f
Julianmora2315 <julian.mora2315@gmail.com>
2026-03-16 19:35:00 -0500

    fix database url railway interno

--- database.py
+++ database.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-#DB_URL = f"mysql+pymysql://prueba:prueba@localhost:3306/prueba"
-DB_URL = f"mysql+pymysql://root:PWfEJaAHkZhtImZlvLFdsjexTTkCLfAI@gondola.proxy.rlwy.net:173523/railway"
+DB_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:PWfEJaAHkZhtImZlvLFdsjexTTkCLfAI@mysql.railway.internal:3306/railway")
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 def get_db():
     db = SessionLocal()
     try:
